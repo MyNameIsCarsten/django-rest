c0b593a3df73dd2e65292db728f19f5cc711e42a
Carsten Linder <Carsten@Carsten-Linder.de>
2023-09-12 22:56:50 +0200

    Included fetch API call in async function

--- todo/static/script.js
+++ todo/static/script.js
@@ -16,18 +16,37 @@ function getCookie(name) {
 }
 const csrftoken = getCookie('csrftoken');
 
-// Example of fetching data from our api
-fetch("/todos/api/1/", {
-    method: "get",
-    headers: {
-        "X-CSRFToken": getCookie("csrftoken"),
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    },
-}).then(function(response) {
-    return response.json();
-}).then(function(data) {
-    console.log("Data is ok", data); // log data to console
-}).catch(function(ex) {
-    console.log("parsing failed", ex); // log error to console
-});
+// Example of fetching data from our api using async function
+async function fetchData() {
+    try {
+      // Send a GET request to the specified URL with headers
+      // Wait until we get a response from fetch
+      const response = await fetch("/todos/api/1/", {
+        method: "get",
+        headers: {
+          "X-CSRFToken": getCookie("csrftoken"), // Include CSRF token in headers
+          "Accept": "application/json", // Specify that we want JSON response
+          "Content-Type": "application/json", // Specify JSON as content type
+        },
+      });
+  
+      // Check if the response status is not OK (e.g., 404 or 500)
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
+      // Parse the JSON response body:
+      // response.json() method is another asynchronous operation
+      // we pause the execution of fetchData until the Promise returned by response.json() is resolved
+      const data = await response.json();
+  
+      // Log the retrieved data to the console
+      console.log("Data is ok", data);
+    } catch (ex) {
+      // Handle any errors that occur during the fetch or parsing
+      console.log("Fetching data failed", ex);
+    }
+  }
+  
+  // Call the async function to fetch data
+  fetchData();
--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -124,8 +124,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-print(BASE_DIR)
-print(os.path.join(BASE_DIR, 'static'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
